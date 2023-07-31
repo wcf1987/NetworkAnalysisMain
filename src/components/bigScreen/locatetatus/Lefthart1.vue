a4b2243152a2b41daf6bef8e805922f15b47e88e
icefish <181712814@qq.com>
2023-07-31 17:10:45 +0800

    增加vue文件css隔离区

---
+++ src/components/bigScreen/locatetatus/Lefthart1.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="chart1">
+
+
+      <dv-decoration7 style="width:150px;height:30px;margin:5% auto">
+      <div class="fontcolor">
+        无人机状态
+      </div>
+
+
+    </dv-decoration7>
+         <div class="fontcolor" style="margin-left: 10%;margin-top: 1%">
+               <div style="float:left;">当前运行状态：</div><div style="color: green;text-align: left;">正常</div>
+               <div style="float:left;">经度：</div><div style="color: #5BB8F1;text-align: left;">东经182.15678</div>
+                <div style="float:left;">纬度：</div><div style="color: #5BB8F1;text-align: left;">北纬38.5678</div>
+               <div >信号强度：</div><div style="color: red;text-align: center;font-size: 40px">高</div>
+                <div style="float:left;">当前时间：</div><div style="color: #3EFFAD;font-style: italic;text-align: left;">2023-7-31 12:21:32</div>
+             </div>
+
+  </div>
+</template>
+
+<script  setup>
+    import {reactive, ref,onMounted ,watch} from 'vue'
+
+</script>
+
+<style lang="less" scoped>
+.chart1{
+  width: 100%;
+  height: 33%;
+
+  display: flex;
+  flex-direction: column;
+    .fontcolor{
+        line-height: 40px;
+    }
+  .chart-header {
+    height: 20px;
+    line-height: 20px;
+    font-size: 16px;
+    text-indent: 20px;
+
+  }
+
+  .chart-lable {
+    margin-top: 5%;
+    height: 40px;
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+    text-indent: 20px;
+    color: #cccdd9;
+    span {
+      color: #096dd9;
+      font-weight: bold;
+      font-size: 35px;
+      margin-left: 20px;
+    }
+  }
+
+  .chart-details{
+    margin-top: 2%;
+    flex: 1;
+  }
+}
+</style>
---
+++ src/components/bigScreen/locatetatus/index.vue
@@ -0,0 +1,152 @@
+<template>
+  <dv-full-screen-container class="box">
+    <!-- 头部标题 -->
+    <div>
+      <div class="title">
+        <span class="title-text">位置管理</span>
+      </div>
+    </div>
+    <!-- 中间 -->
+    <div class="content-main">
+      <!-- 左侧车辆状态框 -->
+      <div class="content-left">
+           <dv-border-box1 style="width: 90%;height: 80%; margin-left: 10%;margin-top: 5%;">
+           <LeftChart1 />
+     </dv-border-box1>
+      </div>
+      <!-- 中间 -->
+      <div class="content-mid">
+        <div class="car">
+        </div>
+      </div>
+      <!-- 右侧 -->
+      <div class="content-right">
+        <dv-border-box1 style="width: 98%;height: 40%;margin-top: 1%">
+           <RightChart1 />
+        </dv-border-box1>
+        <dv-border-box1 style="width: 98%;height: 40%; margin-top: 1%;">
+         <RightChart2 />
+        </dv-border-box1>
+      </div>
+
+    </div>
+    <!-- 底部 -->
+    <div class="content-bottom">
+         <Bottom1 />
+    </div>
+
+  </dv-full-screen-container>
+</template>
+
+<script  setup>
+ import RightChart1 from '@/components/bigScreen/UAVstatus/Righthart1.vue'
+ import RightChart2 from '@/components/bigScreen/UAVstatus/Righthart2.vue'
+ import LeftChart1 from '@/components/bigScreen/UAVstatus/Lefthart1.vue'
+ import Bottom1 from '@/components/bigScreen/UAVstatus/Bottom1.vue'
+</script>
+
+<style lang="less" scoped>
+.box {
+  width: 96% !important;
+  height: 100% !important;
+  background-image: url('./img/bg.png');
+  background-size: 100% 100%;
+  box-shadow: 0 0 3px blue;
+  display: flex;
+  flex-direction: column;
+  margin-left: 64px;
+
+  .title {
+    position: relative;
+    text-align: center;
+    background-size: cover;
+    color: transparent;
+    height: 60px;
+    line-height: 46px;
+    margin-top: 6px;
+
+    .title-text {
+      font-size: 25px;
+      font-weight: 900;
+      letter-spacing: 8px;
+      width: 100%;
+      background: linear-gradient(92deg, #0072FF 0%, #00EAFF 48.8525390625%, #01AAFF 100%);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+  }
+
+  .content-main {
+    width: 100%;
+    height: 70%;
+    display: flex;
+    flex-direction: row;
+
+    .content-left {
+      width: 25%;
+      height: 100%;
+    }
+
+    .content-mid {
+      margin-left: 1%;
+      width: 50%;
+      height: 100%;
+
+      .car {
+        width: 100%;
+        height: 100%;
+        background-image: url('./img/fly.png');
+        background-size: 90%;
+        background-repeat: no-repeat;
+      }
+    }
+
+    .content-right {
+      width: 25%;
+      height: 100%;
+      margin-right: 20px;
+    }
+  }
+
+  .content-bottom {
+    width: 100%;
+    height: 20%;
+    position: relative;
+    bottom: 10px;
+  }
+
+  .bg {
+    width: 100%;
+    height: 60%;
+    top: 20%;
+    left: 4%;
+    position: absolute;
+    background-image: url('./img/bottom.png');
+    background-size: 90% 100%;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .box-item {
+    width: 15%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    // background-color: rgb(255, 153, 0);
+  }
+  .icon {
+    width: 65px;
+    height: 65px;
+    background-image: url('./img/icon-bg.png');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    margin-left: 10%;
+    margin-top: 20px;
+  }
+.fontcolor{
+  color: #f9fff6;
+}
+
+}
+</style>
