0cfab7b1123b8438cde5823a9d72999a212a676d
Muratjan99 <1754371975@qq.com>
2023-08-01 20:00:38 +0800

    首页

--- src/components/bigScreen/systemstatus/LeftBottom.vue
+++ src/components/bigScreen/systemstatus/LeftBottom.vue
@@ -1,58 +1,27 @@
 <template>
     <div class="timers digtal_font_time">
         <dv-border-box-7 style="width: auto;height:70px;">
-            <p>无人机：</p>
+            <div style="display: flex; flex-direction: row;">
+                <div class="text_b">无人机1</div>
+                <dv-button class="bot" border="Border3" color="#c8161d" font-color="#e18a3b">掉线</dv-button>
+            </div>
         </dv-border-box-7>
         <dv-border-box-7 style="width: auto;height:70px;">
-            <p>无人机：</p>
+            <div style="display: flex; flex-direction: row;">
+                <div class="text_b">无人机2</div>
+                <dv-button class="bot" border="Border3" color="#4c8045" font-color="#4c8045">在线</dv-button>
+            </div>
         </dv-border-box-7>
         <dv-border-box-7 style="width: auto;height:70px;">
-            <p>无人机</p>
+            <div style="display: flex; flex-direction: row;">
+                <div class="text_b">无人机3</div>
+                <dv-button class="bot" border="Border3" color="#4c8045" font-color="#4c8045">在线</dv-button>
+            </div>
         </dv-border-box-7>
     </div>
 </template>
 <script>
-import { formatTime } from "./utils.js";
 export default {
-    data() {
-        return {
-            timing: null,
-            loading: true,
-            dateDay: null,
-            dateYear: null,
-            dateWeek: null,
-            weekday: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
-        };
-    },
-    filters: {
-        numsFilter(msg) {
-            return msg || 0;
-        },
-    },
-    computed: {},
-    created() { },
-    mounted() {
-        this.timeFn();
-        this.cancelLoading();
-    },
-    methods: {
-        showSetting() {
-            this.$refs.setting.init();
-        },
-        timeFn() {
-            this.timing = setInterval(() => {
-                this.dateDay = formatTime(new Date(), "HH: mm: ss");
-                this.dateYear = formatTime(new Date(), "yyyy-MM-dd");
-                this.dateWeek = this.weekday[new Date().getDay()];
-            }, 1000);
-        },
-        cancelLoading() {
-            let timer = setTimeout(() => {
-                this.loading = false;
-                clearTimeout(timer);
-            }, 500);
-        },
-    },
 };
 </script>
 
@@ -74,4 +43,20 @@ export default {
     text-align: center;
     font-size: 20px;
     line-height: 50px;
-}</style>
+}
+
+.text_b {
+    margin: 0;
+    padding: 0;
+    margin-left: 20px;
+    text-align: center;
+    font-size: 20px;
+    line-height: 50px;
+}
+.bot{
+    width:80px;
+    height: 40px;
+    margin-top: 10px;
+    margin-left: 200px;
+}
+</style>
